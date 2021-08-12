197e5fd400d62bfdd7547311e1c302e76ccdd244
Steve Goode <steve@madhouseminers.com>
2021-08-12 08:48:11 +0100

    Initial version

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import { ApolloServer } from "apollo-server";
 import { ApolloGateway } from "@apollo/gateway";
 
 const gateway = new ApolloGateway({
-  serviceList: [
-    {
-      name: "login",
-      url: "http://login.madhouseminers.svc.cluster.local:4000",
-    },
-  ],
+  serviceList: [{ name: "login", url: "http://login:4000" }],
 });
 const server = new ApolloServer({ gateway });
 
-server.listen().then(({ url }) => console.log(`${url}`));
+server
+  .listen()
+  .then(({ url }) => console.log(`${url}`))
+  .catch((err) => {
+    console.error(err);
+  });
